acf80a18156b45f02dd3d54af0ef2cd10b5391c8
Justin Wong <justinryanwong@berkeley.edu>
2020-07-29 22:56:15 +0000

    part 1 and 2 done with 84% code covered

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,13 +1,19 @@
 class MoviesController < ApplicationController
-
+  
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
+    redirect_to directed_by if id == 'directed_by'
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+  end
+  
+  def directed_by
+    director = params[:director]
+    @movies = Movie.where("director = ?", director)
   end
 
   def index
@@ -60,5 +66,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  
 end
